0c713e95668b8db12ed53ed979aa5f1da9282810
Saqif Badruddin <saqif280@gmail.com>
2018-06-25 22:50:52 -0400

    typewriter resume

---
+++ resume/main.js
@@ -0,0 +1,45 @@
+$(function () {
+  var typewriter = new Typewriter($('#position'));
+  // typewriter.typeText('Design Engineer');
+  animate(typewriter);
+});
+
+function calculateSpaces(){
+  var positions = ['Design Engineer','Front-end Engineer','Web Developer','Software Engineer'];
+  var sequenceText = ['','','',''];
+  for(i = 0; i<sequenceText.length; i++){
+    var text = '';
+    var numBackspaces = (i == 0)
+                      ? positions[sequenceText.length-1].length
+                      : positions[i-1].length;
+    for(j=0; j<numBackspaces; j++){
+      text += '\\b';
+    }
+    text += positions[i];
+    console.log(text);
+  }
+}
+// calculateSpaces();
+
+function animate(typewriter) {
+  sequence = [
+    {
+      text: '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bDesign Engineer',
+      delayAfter: 500
+    },
+    {
+      text: '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bFront-end Engineer',
+      delayAfter: 500
+    },
+    {
+      text: '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bWeb Developer',
+      delayAfter: 500
+    },
+    {
+      text: '\b\b\b\b\b\b\b\b\b\b\b\b\bSoftware Engineer',
+      delayAfter: 500,
+      callback: function () { animate(typewriter) }
+    }
+  ];
+  typewriter.playSequence(sequence);
+}
